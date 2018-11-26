2048e411cdd9ecb645b39017dd6ff0bded4e8dd4
root <root@localhost.localdomain>
2018-11-26 15:13:39 +0900

    jenkins対応

--- cmd.sh
+++ cmd.sh
@@ -4,7 +4,11 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Runnning Development server"
     exec python "identidock.py"
-  else
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Runnning UnitTest"
+    exec python "tests.py"
+    
+else
     echo "Runnning Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
